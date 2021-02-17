9fd81c09f53442c467a62a4497b7bc60abf5db8f
simonelgarrad <simugarrad@gmail.com>
2021-02-17 14:53:46 +0530

    Updated

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint=Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var slingshot;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,6 +38,10 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    
+
+    slingshot= new SlingShot(bird.body,{x:200,y:100});
+
 }
 
 function draw(){
@@ -61,4 +67,15 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    
+    slingshot.display();
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY});
+}
+
+function mouseReleased(){
+    slingshot.fly();
 }
